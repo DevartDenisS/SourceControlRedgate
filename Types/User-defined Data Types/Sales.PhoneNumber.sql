39822ccb58773830be298e44d50719d3ac29fab1
DevartDenisS <132042766+DevartDenisS@users.noreply.github.com>
2025-04-25 12:56:04 +0300

    Delete Types directory

--- Types/User-defined Data Types/Sales.PhoneNumber.sql
+++
@@ -1,4 +0,0 @@
-CREATE TYPE [Sales].[PhoneNumber] FROM nvarchar (20) NOT NULL
-GO
-EXEC sp_addextendedproperty N'MS_Description', N'Custom type for phone numbers', 'SCHEMA', N'Sales', 'TYPE', N'PhoneNumber', NULL, NULL
-GO
